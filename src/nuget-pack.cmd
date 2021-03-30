04271250f684650e070a1451338850d35f7a8b63
Maxim Shoshin <max.shoshin@gmail.com>
2021-03-31 00:38:55 +0300

    Fix IconUrl, remove unnecessary files, correct nuget dependencies

--- src/nuget-pack.cmd
+++
@@ -1,1 +0,0 @@
-powershell.exe -ExecutionPolicy Bypass -NoProfile -NonInteractive -File nuget-pack.ps1
--- src/nuget-pack.ps1
+++
@@ -1,9 +0,0 @@
-try {
-        cd LoadingIndicator.WinForms\bin\Release
-	$version = (gi LoadingIndicator.WinForms.dll).VersionInfo.ProductVersion
-	echo "Building package with version $version."
-	& '..\..\..\packages\NuGet.CommandLine.4.9.2\tools\NuGet.exe' pack LoadingIndicator.WinForms.nuspec -Version $version -OutputDirectory .
-}
-catch {
-	exit 1
-}
